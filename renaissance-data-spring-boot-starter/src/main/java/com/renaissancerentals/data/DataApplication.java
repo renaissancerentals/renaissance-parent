566528898e17d7f67e1e63860f1c5ef6dc12057c
contentmuncher <contentmunch@gmail.com>
2025-08-18 20:52:35 -0400

    feat: add renaissance data

---
+++ renaissance-data-spring-boot-starter/src/main/java/com/renaissancerentals/data/DataApplication.java
@@ -0,0 +1,12 @@
+package com.renaissancerentals.data;
+
+import com.renaissancerentals.foundation.FoundationApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FoundationApplication.class, args);
+    }
+}
