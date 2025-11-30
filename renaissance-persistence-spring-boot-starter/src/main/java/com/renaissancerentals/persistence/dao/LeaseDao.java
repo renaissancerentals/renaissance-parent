cc593e8bf181901c20f514e94449b335a64db297
contentmuncher <contentmunch@gmail.com>
2025-11-30 16:15:00 -0500

    feat: lease dao

--- renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/LeaseDao.java
+++ renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/LeaseDao.java
@@ -2,21 +2,11 @@ package com.renaissancerentals.persistence.dao;
 
 import java.util.List;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.renaissancerentals.persistence.entity.LeaseEntity;
 
 public interface LeaseDao extends CrudRepository<LeaseEntity, Long> {
 
-    List<LeaseEntity> findByUnitIdOrderByStartDateDesc(String unitId);
-
-    List<LeaseEntity> findByUnitIdOrderByEndDateDesc(String unitId);
-
-    @Query("""
-            SELECT * from lease
-            WHERE end_date >=CURRENT_DATE
-            ORDER BY end_date DESC
-            """)
-    List<LeaseEntity> findAllActiveLeasesOrderByEndDateDesc();
+    List<LeaseEntity> findAllOrderByEndDateDesc();
 }
