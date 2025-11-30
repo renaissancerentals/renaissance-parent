fa56b8ee1e89cde53fc0f340fbfcdca4a5a24ffb
contentmuncher <contentmunch@gmail.com>
2025-11-30 13:13:30 -0500

    chore: typo in sql

--- renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/LeaseDao.java
+++ renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/LeaseDao.java
@@ -1,10 +1,11 @@
 package com.renaissancerentals.persistence.dao;
 
-import com.renaissancerentals.persistence.entity.LeaseEntity;
+import java.util.List;
+
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
+import com.renaissancerentals.persistence.entity.LeaseEntity;
 
 public interface LeaseDao extends CrudRepository<LeaseEntity, Long> {
 
@@ -14,7 +15,7 @@ public interface LeaseDao extends CrudRepository<LeaseEntity, Long> {
 
     @Query("""
             SELECT * from lease
-            WHERE end_data >=CURRENT_DATE
+            WHERE end_date >=CURRENT_DATE
             ORDER BY end_date DESC
             """)
     List<LeaseEntity> findAllActiveLeasesOrderByEndDateDesc();
