00483383cb00b8b12ca9502be8eaee5bb2ddd884
contentmuncher <contentmunch@gmail.com>
2025-12-07 11:09:49 -0500

    feat: amenity by floorlan id

--- renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/AmenityDao.java
+++ renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/AmenityDao.java
@@ -1,9 +1,12 @@
 package com.renaissancerentals.persistence.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.renaissancerentals.persistence.entity.AmenityEntity;
 
 public interface AmenityDao extends CrudRepository<AmenityEntity, Long> {
-
+    List<AmenityEntity> findByFloorplanId(@Param("floorplanId") String floorplanId);
 }
