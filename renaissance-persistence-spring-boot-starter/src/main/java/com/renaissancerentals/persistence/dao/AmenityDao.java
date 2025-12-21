bd439446b37d1363980e71818c213424bc1659c5
contentmuncher <contentmunch@gmail.com>
2025-12-21 17:41:53 -0500

    feat: add query for distict amenity list

--- renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/AmenityDao.java
+++ renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/AmenityDao.java
@@ -2,6 +2,7 @@ package com.renaissancerentals.persistence.dao;
 
 import java.util.List;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -9,4 +10,11 @@ import com.renaissancerentals.persistence.entity.AmenityEntity;
 
 public interface AmenityDao extends CrudRepository<AmenityEntity, Long> {
     List<AmenityEntity> findByFloorplanId(@Param("floorplanId") String floorplanId);
+
+    @Query("""
+            SELECT DISTINCT type, name
+                FROM amenity
+                ORDER BY type, name
+            """)
+    List<AmenityEntity> findDistinctTypeAndName();
 }
