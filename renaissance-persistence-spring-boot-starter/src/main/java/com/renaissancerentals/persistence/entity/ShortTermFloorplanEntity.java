91830f42f3bea6d47c3acd5b2a6423b6e7034bb0
contentmuncher <contentmunch@gmail.com>
2025-12-21 10:07:43 -0500

    feat: include column name for numeric field names

--- renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/entity/ShortTermFloorplanEntity.java
+++ renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/entity/ShortTermFloorplanEntity.java
@@ -3,6 +3,7 @@ package com.renaissancerentals.persistence.entity;
 import java.io.Serializable;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.Data;
@@ -16,14 +17,19 @@ public class ShortTermFloorplanEntity implements Serializable, FloorplanAware {
     @Id
     private Long id;
 
+    @Column("price_for2to4days")
     private String priceFor2To4Days;
 
+    @Column("price_for5to13days")
     private String priceFor5To13Days;
 
+    @Column("price_for14to29days")
     private String priceFor14To29Days;
 
+    @Column("price_for1to4months")
     private String priceFor1To4Months;
 
+    @Column("price_for4and_more_months")
     private String priceFor4andMoreMonths;
 
     private Integer squareFoot;
