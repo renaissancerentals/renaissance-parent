2b9239d50292b9a341e666134397fbd638028430
contentmuncher <contentmunch@gmail.com>
2026-01-17 22:32:51 -0500

    feat: findBy Property Id

--- renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/PropertyAmenityDao.java
+++ renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/PropertyAmenityDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.renaissancerentals.persistence.entity.PropertyAmenityEntity;
 
@@ -15,4 +16,6 @@ public interface PropertyAmenityDao extends CrudRepository<PropertyAmenityEntity
                 ORDER BY type, name
             """)
     List<PropertyAmenityEntity> findDistinctTypeAndName();
+
+    List<PropertyAmenityEntity> findByPropertyId(@Param("propertyId") String propertyId);
 }
