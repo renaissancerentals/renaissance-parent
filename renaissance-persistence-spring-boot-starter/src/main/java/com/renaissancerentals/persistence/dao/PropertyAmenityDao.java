d9ee19d9ecdd831562d9a2357758a0a0a8d236d0
contentmuncher <contentmunch@gmail.com>
2026-01-17 20:08:05 -0500

    feat: add property amenity list

--- renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/PropertyAmenityDao.java
+++ renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/PropertyAmenityDao.java
@@ -1,9 +1,18 @@
 package com.renaissancerentals.persistence.dao;
 
+import java.util.List;
+
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.renaissancerentals.persistence.entity.PropertyAmenityEntity;
 
 public interface PropertyAmenityDao extends CrudRepository<PropertyAmenityEntity, Long> {
 
+    @Query("""
+            SELECT DISTINCT type, name
+                FROM property_amenity
+                ORDER BY type, name
+            """)
+    List<PropertyAmenityEntity> findDistinctTypeAndName();
 }
