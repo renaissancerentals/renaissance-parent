2c5e0bba8ea82aadd4cc75ad2c200aba3f23dab6
contentmuncher <contentmunch@gmail.com>
2026-01-24 14:07:06 -0500

    feat: sublet queries

--- renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/SubletDao.java
+++ renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/SubletDao.java
@@ -1,8 +1,11 @@
 package com.renaissancerentals.persistence.dao;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -16,4 +19,13 @@ public interface SubletDao extends CrudRepository<SubletEntity, Long> {
 
     Optional<SubletEntity> findOneByAssetKey(@Param("assetKey") String assetKey);
 
+    @Query("SELECT * FROM sublet WHERE cover_image = ''")
+    List<SubletEntity> findByCoverImageEmpty();
+
+    @Query("SELECT * FROM sublet WHERE available_to <= :availableTo OR created_date<=:createdDate")
+    List<SubletEntity> findExpired(@Param("availableTo") LocalDate availableTo,
+            @Param("createdDate") LocalDateTime createdDate);
+
+    @Query("SELECT * FROM sublet WHERE active = false AND created_date<=:createdDate")
+    List<SubletEntity> findExpiredInactive(@Param("createdDate") LocalDateTime createdDate);
 }
