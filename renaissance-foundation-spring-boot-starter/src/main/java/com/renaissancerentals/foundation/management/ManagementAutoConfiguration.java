671dae2aa982f56ea17c29616f0cd7d0ccf40465
contentmuncher <contentmunch@gmail.com>
2026-01-31 00:10:18 -0500

    feat: include management base-path by default

---
+++ renaissance-foundation-spring-boot-starter/src/main/java/com/renaissancerentals/foundation/management/ManagementAutoConfiguration.java
@@ -0,0 +1,10 @@
+package com.renaissancerentals.foundation.management;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration(proxyBeanMethods = false)
+@PropertySource("classpath:renaissance-foundation-defaults.properties")
+public class ManagementAutoConfiguration {
+
+}
