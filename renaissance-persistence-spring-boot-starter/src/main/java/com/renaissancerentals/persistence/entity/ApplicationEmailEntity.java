9a0278a83eb742bd03a1c5a2d6b643b9c6e822eb
contentmuncher <contentmunch@gmail.com>
2026-02-08 22:19:14 -0500

    feat: make application email persistable

--- renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/entity/ApplicationEmailEntity.java
+++ renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/entity/ApplicationEmailEntity.java
@@ -6,13 +6,15 @@ import java.util.UUID;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.Data;
 
 @Data
 @Table(name = ApplicationEmailEntity.TABLE_NAME)
-public class ApplicationEmailEntity implements Serializable, RenaissanceEmailAware {
+public class ApplicationEmailEntity implements Serializable, RenaissanceEmailAware, Persistable<UUID> {
     public static final String TABLE_NAME = "application_email";
 
     @Id
@@ -29,4 +31,15 @@ public class ApplicationEmailEntity implements Serializable, RenaissanceEmailAwa
     @CreatedDate
     private Instant createdAt;
 
+    @Transient
+    private boolean isNew;
+
+    @Override
+    public boolean isNew(){
+        return isNew;
+    }
+
+    public void markNew(){
+        this.isNew = true;
+    }
 }
