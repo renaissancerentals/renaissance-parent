e1fb47c10c51fe9dcda00d6007c3cfca94ca2e1e
contentmuncher <contentmunch@gmail.com>
2026-02-11 14:45:54 -0500

    feat: all active unit

--- renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/UnitDao.java
+++ renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/UnitDao.java
@@ -1,9 +1,16 @@
 package com.renaissancerentals.persistence.dao;
 
+import java.util.List;
+
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.renaissancerentals.persistence.entity.UnitEntity;
 
 public interface UnitDao extends CrudRepository<UnitEntity, String> {
 
+    @Query(value = "SELECT * FROM unit u \n" + "    JOIN floorplan f ON f.id=u.floorplan_id\n"
+            + "    JOIN property p ON p.id=f.property_id \n"
+            + "WHERE u.active=true AND f.active=true AND p.active=true")
+    List<UnitEntity> findAllActive();
 }
