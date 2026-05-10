b0fe099f16b599ef6728b38a352c5b511296bf8e
contentmuncher <contentmunch@gmail.com>
2026-05-09 23:08:42 -0400

    chore: refactor link -> url

--- renaissance-api-spring-boot-starter/src/main/java/com/renaissancerentals/api/domain/HomePageLinkedImageRegion.java
+++ renaissance-api-spring-boot-starter/src/main/java/com/renaissancerentals/api/domain/HomePageLinkedImageRegion.java
@@ -1,4 +1,4 @@
 package com.renaissancerentals.api.domain;
 
-public record HomePageLinkedImageRegion(double x, double y, double width, double height, String link) {
+public record HomePageLinkedImageRegion(double x, double y, double width, double height, String url) {
 }
--- renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/entity/LinkedImageRegion.java
+++ renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/entity/LinkedImageRegion.java
@@ -1,4 +1,4 @@
 package com.renaissancerentals.persistence.entity;
 
-public record LinkedImageRegion(double x, double y, double width, double height, String link) {
+public record LinkedImageRegion(double x, double y, double width, double height, String url) {
 }
