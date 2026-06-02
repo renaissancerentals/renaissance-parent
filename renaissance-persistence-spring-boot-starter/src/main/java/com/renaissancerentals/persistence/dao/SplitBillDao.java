c2e083dc1175ed1a33aac7716922dc482f5fc8be
contentmuncher <contentmunch@gmail.com>
2026-06-02 13:31:09 -0400

    feat: add insert/update

--- renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/SplitBillDao.java
+++ renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/dao/SplitBillDao.java
@@ -1,8 +1,30 @@
 package com.renaissancerentals.persistence.dao;
 
 import com.renaissancerentals.persistence.entity.SplitBillEntity;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SplitBillDao extends CrudRepository<SplitBillEntity, Long> {}
+public interface SplitBillDao extends CrudRepository<SplitBillEntity, Long> {
+
+    @Modifying
+    @Query(
+            """
+            INSERT INTO split_bill (owner_data, last_modified_date)
+            VALUES (:ownerData::jsonb, CURRENT_TIMESTAMP)
+            """)
+    void insert(@Param("ownerData") String ownerData);
+
+    @Modifying
+    @Query(
+            """
+            UPDATE split_bill
+            SET owner_data         = :ownerData::jsonb,
+                last_modified_date = CURRENT_TIMESTAMP
+            WHERE id = :id
+            """)
+    void update(@Param("id") Long id, @Param("ownerData") String ownerData);
+}
--- renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/entity/SplitBillEntity.java
+++ renaissance-persistence-spring-boot-starter/src/main/java/com/renaissancerentals/persistence/entity/SplitBillEntity.java
@@ -4,17 +4,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("split_bill")
 @Builder
-public record SplitBillEntity(
-        @Id Long id,
-        OwnerData ownerData,
-        @LastModifiedBy String lastModifiedBy,
-        @LastModifiedDate LocalDateTime lastModifiedDate) {
+public record SplitBillEntity(@Id Long id, OwnerData ownerData, @LastModifiedDate LocalDateTime lastModifiedDate) {
 
     public record OwnerData(String name, int unitCount, List<OwnerClassData> classes) {
 
